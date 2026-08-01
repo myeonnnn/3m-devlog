c959d477db2b85d8d9c2f214e0a7458926fb78d3
tony <johnkim994@naver.com>
2026-08-01 13:48:52 +0900

    feat(view): insight 배럴(index.ts) 추가

---
+++ view/src/features/insight/index.ts
@@ -0,0 +1,6 @@
+export { InsightEntryButton } from './components/insight-entry-button';
+export { InsightModal } from './components/insight-modal';
+export { InsightHistoryCard } from './components/insight-history-card';
+export { useInsightController } from './hooks/use-insight-controller';
+export { useLatestInsightsQuery } from './hooks/use-insights';
+export type { Insight, InsightPeriodType, LatestInsights } from './types';
